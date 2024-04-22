52cb5de1dd36e182e6c2c03f8f23524a1a61e49e
Carlos Anriquez <canriquez@gmail.com>
2024-04-22 11:17:54 -0300

    first commit

---
+++ prueba_exports/librerias/utiles/suma.js
@@ -0,0 +1,11 @@
+export function separador() {
+    console.log('=======================================')
+}
+
+export function suma(a, b) {
+    console.log(` (${a} + ${b}) = `, a+b)
+}
+
+export default function multipliacion(a,b) {
+    console.log('la multiplicacion es: ', a*b);
+}
---
+++ prueba_exports/script.js
@@ -0,0 +1,33 @@
+//import suma, { separador } from './librerias/utiles/suma.js'
+import multiplicacion, * as utiles from './librerias/utiles/suma.js'
+
+console.log('VAMOS A USAR LA FUNCION')
+
+utiles.suma(11,11)
+
+utiles.separador()
+utiles.suma(11,11)
+
+utiles.separador()
+
+utiles.suma(11,11)
+
+utiles.separador()
+utiles.suma(11,11)
+
+utiles.separador()
+utiles.suma(11,11)
+
+utiles.separador()
+utiles.suma(11,11)
+
+utiles.separador()
+utiles.suma(11,11)
+
+utiles.separador()
+utiles.suma(11,11)
+
+utiles.separador()
+
+
+multiplicacion(11,11)
